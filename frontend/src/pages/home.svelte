4454e5bc04a17dbd7186f9573707103cbd5b752b
exoticknight <draco.knight0@gmail.com>
2025-06-26 17:18:12 +0800

    feat: restructure app routing and add landing and table pages
    - Updated routing configuration to include a new landing page and a table page.
    - Implemented pre-route hooks to manage user state and redirect if necessary.
    - Removed the old home page component and created a new landing page with navigation to the table.
    - Added a new table page component.
    - Introduced a user state store to manage user-related data.
    - Updated global styles and main entry point to reflect new structure.
    - Configured TypeScript settings for improved module detection and project structure.
    - Integrated Tailwind CSS into the Vite configuration.
    - Added VSCode settings for ESLint and Prettier configurations.

--- frontend/src/pages/home.svelte
+++
@@ -1,9 +0,0 @@
-<script lang>
-  import { onMount } from "svelte";
-
-  onMount(() => {
-    console.log("Home component mounted");
-  });
-</script>
-
-<div>Home</div>
---
+++ frontend/src/pages/landing.svelte
@@ -0,0 +1,13 @@
+<script lang>
+  import { goto } from '@mateothegreat/svelte5-router'
+  import { onMount } from 'svelte'
+
+  onMount(() => {
+    console.log('Home component mounted')
+  })
+</script>
+
+<div>
+  Home
+  <button class='btn' type='button' on:click={() => goto('/#table')}>Go to Table</button>
+</div>

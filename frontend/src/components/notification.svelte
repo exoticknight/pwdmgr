d94fd9b12d1429817ca5d2e52a3876ce40056f7c
exoticknight <draco.knight0@gmail.com>
2025-08-18 19:47:35 +0800

    fix: update toast container position for improved visibility

--- frontend/src/components/notification.svelte
+++ frontend/src/components/notification.svelte
@@ -21,7 +21,7 @@
 
 <!-- Toast container -->
 {#if notifications.length > 0}
-  <div class='toast toast-bottom toast-end z-50'>
+  <div class='toast toast-top toast-center z-50'>
     {#each notifications as notification (notification.id)}
       <div class='alert {getAlertClass(notification.type)} shadow-lg'>
         <span class='text-white'>{notification.message}</span>
